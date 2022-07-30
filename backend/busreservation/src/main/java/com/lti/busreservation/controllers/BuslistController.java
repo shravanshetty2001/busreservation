b2b8923f99417b41ed647efd9c1d3fbfe3b58c2d
Ankitsingh0751 <80910681+Ankitsingh0751@users.noreply.github.com>
2022-07-30 12:52:25 +0530

    Update BuslistController.java

---
+++ backend/busreservation/src/main/java/com/lti/busreservation/controllers/BuslistController.java
@@ -0,0 +1,35 @@
+package com.lti.busreservation.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.busreservation.models.Buslist;
+import com.lti.busreservation.repository.BuslistRepository;
+
+@RestController
+public class BuslistController {
+	@Autowired
+	private BuslistRepository buslistRepository;
+	
+	@GetMapping("/Buslist")
+	public List<Buslist> getBuslist()
+	{
+		return buslistRepository.findAll();
+	}
+  
+  @PostMapping
+  public Buslist createBuslist(@Valid @RequestBody Buslist bs)
+  {
+      return buslistRepository.save(bs);
+  }
+	
+	
+	
+}
