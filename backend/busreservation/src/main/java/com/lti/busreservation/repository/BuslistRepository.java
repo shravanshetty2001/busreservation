b281b5e60b3aa2bc65728ce73dfd81e371dbb700
Ankitsingh0751 <80910681+Ankitsingh0751@users.noreply.github.com>
2022-07-31 21:21:38 +0530

    Update BuslistRepository.java

--- backend/busreservation/src/main/java/com/lti/busreservation/repository/BuslistRepository.java
+++ backend/busreservation/src/main/java/com/lti/busreservation/repository/BuslistRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lti.busreservation.models.Buslist;
 
-public interface BuslistRepository extends JpaRepository<Buslist, Long>{
+public interface BuslistRepository extends JpaRepository<Buslist, Integer>{
+
+	
+	
 
 	
 
