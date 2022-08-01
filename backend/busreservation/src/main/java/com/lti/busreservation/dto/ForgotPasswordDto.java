2ec165266df1678162e069c7e02b56b6c9137566
shravanshetty2001 <shravanshetty2001@gmail.com>
2022-08-01 15:54:49 +0530

    Updated files

---
+++ backend/busreservation/src/main/java/com/lti/busreservation/dto/ForgotPasswordDto.java
@@ -0,0 +1,15 @@
+package com.lti.busreservation.dto;
+
+public class ForgotPasswordDto {
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
--- backend/busreservation/src/main/java/com/lti/busreservation/service/AdminloginserviceImpl.java
+++ backend/busreservation/src/main/java/com/lti/busreservation/service/AdminloginserviceImpl.java
@@ -3,13 +3,20 @@ package com.lti.busreservation.service;
 
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.lti.busreservation.dto.AdminDto;
 import com.lti.busreservation.dto.AdminloginDto;
 import com.lti.busreservation.dto.AdminregisterDto;
 import com.lti.busreservation.dto.AdminstatusDto;
+import com.lti.busreservation.dto.ForgotPasswordDto;
 import com.lti.busreservation.models.Admin;
 import com.lti.busreservation.repository.AdminRepository;
 
@@ -17,7 +24,44 @@ import com.lti.busreservation.repository.AdminRepository;
 public class AdminloginserviceImpl implements Adminloginservice {
 	@Autowired
 	private AdminRepository adminRepository;
-
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Override
+	public AdminstatusDto forgotPassword(ForgotPasswordDto forgotpasswordDto)
+	{
+		List<Admin> ad=adminRepository.findAll();
+		AdminstatusDto asd=new AdminstatusDto();
+		for(Admin a:ad)
+		{
+			if(a.getEmail().equals(forgotpasswordDto.getEmail()))
+					{
+						int id=a.getId();
+						try {
+							SendMail(id,a.getEmail());
+							AdminDto as=new AdminDto();
+							as.setId(a.getId());
+							as.setEmail(a.getEmail());
+							as.setPassword(a.getPassword());
+							as.setContactno(a.getContactno());
+							as.setTravelname(a.getTravelname());
+							asd.setStatus(true);
+							asd.setAdmin(as);
+							return asd;
+						} catch (MessagingException e) {
+							// TODO Auto-generated catch block
+							asd.setStatus(false);
+							asd.setErrorMessage(e.getMessage());
+							return asd;
+						}
+						
+					}
+		}
+		asd.setStatus(false);
+		asd.setErrorMessage("Wrong Password");
+		return asd;
+		
+	}
 	@Override
 	public AdminstatusDto verifyData(AdminloginDto adminlogindto)
 	{
@@ -100,4 +144,22 @@ public class AdminloginserviceImpl implements Adminloginservice {
 		asd.setAdmin(as);
 		return asd;
 	}
+	@Override
+		public void SendMail(int id,String email) throws MessagingException
+		{
+			MimeMessage msg=mailSender.createMimeMessage();
+			MimeMessageHelper helper= new MimeMessageHelper(msg);
+			helper.setFrom("shravanshetty2001@gmail.com");
+			helper.setTo(email);
+			String subject="Your BookBus Login Reset Password Link";
+			String content="<p>Hello,</p>"+ "<p>You have requested to reset your password.</p>"
+		            + "<p>Copy paste the link below to change your password:</p>" 
+					+ "<p>https://localhost:4200/resetpassword?id="+id+"</p>";
+			helper.setSubject(subject);
+			helper.setText(content);
+			mailSender.send(msg);
+			
+		}
+		
+
 }
