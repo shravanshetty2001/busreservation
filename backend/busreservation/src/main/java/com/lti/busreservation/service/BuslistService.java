12ca060a883f6a12417e7dd2e22eba4f9e490978
Ankitsingh0751 <80910681+Ankitsingh0751@users.noreply.github.com>
2022-08-02 09:59:06 +0530

    Update BuslistService.java

--- backend/busreservation/src/main/java/com/lti/busreservation/service/BuslistService.java
+++ backend/busreservation/src/main/java/com/lti/busreservation/service/BuslistService.java
@@ -1,11 +1,21 @@
 package com.lti.busreservation.service;
 
+import java.util.List;
+
 import com.lti.busreservation.dto.Buslistdto;
-import com.lti.busreservation.dto.Bustimtabledto;
+import com.lti.busreservation.dto.Bustimetabledto;
+import com.lti.busreservation.dto.UserdetailDto;
+import com.lti.busreservation.dto.UserdetailRegisterDto;
+import com.lti.busreservation.dto.UserdetailStatusDto;
 import com.lti.busreservation.models.Buslist;
 
 public interface BuslistService {
-	public Buslist addBuslist(Buslistdto Buslistdto,Bustimtabledto bustimtabledto);
-	public Buslist getBuslist();
+	public Buslist addBuslist(Buslistdto Buslistdto,Bustimetabledto bustimtabledto);
+	
+	List<Buslistdto> getAllbusdetail();
+	
+
+	Buslistdto addbusdetail(Buslistdto buslistdto);
 	
 }
+
